90ae541684dbd3dfa69f0f3d76166a1f46a06372
paola <paola@LAPTOP-1G3S23NP.gateway.2wire.net>
2017-05-23 16:48:52 -0500

    Iniciando Calculadora

---
+++ src/calculadora/Operaciones.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author paola
+ */
+public class Operaciones {
+    
+    
+    public static double suma( double x ,double y ){
+        return x + y;
+    }
+    public static double resta( double x , double y){
+        return x - y;
+    }
+    
+    
+    
+    
+    
+}
